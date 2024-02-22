cdc1e0364c7eb09eb59506be3828b2bbf9fee515
santoshkedar <santosh.kedar31@gmail.com>
2024-02-22 01:50:47 -0600

    Added MTS TCs

---
+++ src/test/java/santoshkedar/testDevOpsprocess/MTS_Test.java
@@ -0,0 +1,24 @@
+package santoshkedar.testDevOpsprocess;
+
+import org.testng.annotations.Test;
+
+public class MTS_Test 
+{
+	@Test
+	public void MTS_Test_TC1()
+	{
+		System.out.println("MTS_Test_TC1 - Codes to be implemenated later");
+	}
+	
+	@Test
+	public void MTS_Test_TC2()
+	{
+		System.out.println("MTS_Test_TC2 - Codes to be implemenated later");
+	}
+	
+	@Test
+	public void MTS_Test_TC3()
+	{
+		System.out.println("MTS_Test_TC3 - Codes to be implemenated later");
+	}
+}
