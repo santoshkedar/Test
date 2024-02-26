acb2b4018e9fea69e672f9518f01ec279a0921db
santoshkedar <santosh.kedar31@gmail.com>
2024-02-26 00:57:23 -0600

    Added SNF TCs

---
+++ src/test/java/santoshkedar/testDevOpsprocess/SNF_Test.java
@@ -0,0 +1,24 @@
+package santoshkedar.testDevOpsprocess;
+
+import org.testng.annotations.Test;
+
+public class SNF_Test 
+{
+	@Test
+	public void SNF_Test_TC1()
+	{
+		System.out.println("SNF_Test_TC1 - Codes to be implemenated later");
+	}
+	
+	@Test
+	public void SNF_Test_TC2()
+	{
+		System.out.println("SNF_Test_TC2 - Codes to be implemenated later");
+	}
+	
+	@Test
+	public void SNF_Test_TC3()
+	{
+		System.out.println("SNF_Test_TC3 - Codes to be implemenated later");
+	}
+}
